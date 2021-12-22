168e7ca1f85d3b1428fd61aeadd1aa9647aeb9df
Bernard <bernardkrasowski111@gmail.com>
2021-12-22 12:25:34 +0000

    update menu-items to useNavigate

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
-function MenuItem({ title, imageUrl, size }) {
+function MenuItem({ title, imageUrl, size, linkUrl }) {
+  let navigate = useNavigate();
+
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => {
+        console.log(navigate(`${linkUrl}`));
+      }}
+    >
       <div
         className="background-image"
         style={{
